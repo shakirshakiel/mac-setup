cf205794cf5ad62ff62688c2e8c295101a26f600
Shakir Shakiel <shakirshakiel@gmail.com>
2019-06-04 17:50:38 +0530

    Add email option to git

--- git.sh
+++ git.sh
@@ -3,4 +3,5 @@ set -eoux pipefail
 git config --global alias.co checkout
 git config --global alias.st status
 git config --global user.name "Shakir Shakiel"
+git config --global user.email "shakirshakiel@gmail.com"
 
